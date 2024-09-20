6c2b0d68490afb8994c278044e7931f332b5dc7e
javapak24 <ajava2424@gmail.com>
2024-09-20 13:15:37 -0400

    fix login logic to accept correct password

--- index.js
+++ index.js
@@ -186,8 +186,31 @@ app.get('/documentation', (req, res) => {
 * POST create new user
 */
 app.post('/users', async (req, res) => {
-    const newUser = req.body;
-        res.status(201).json(await Users.create(newUser));
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+    .then((user) => {
+      if (user) {
+      //If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
 /**
